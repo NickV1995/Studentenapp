a5ca0e32d37d2cafe78c88abe3355080a7b83f1d
scalda-nvinke <nick_vinke@student.scalda.nl>
2021-03-06 21:37:48 +0100

    Create pdo.php and made pdo connection

---
+++ pdo.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'studentenapp');
+define('DB_USER', 'root');
+define('DB_PASS', 'hgv4kx5y');
+
+function verbinden(){
+  $verbinding = 'mysql:host=' . DB_HOST . ';DB_NAME' . DB_NAME;
+  $db = null;
+}
+
+try{
+  return new PDO($verbinding, DB_USER, DB_PASS);
+} catch(PDOexception $e){
+  return 'De volgende foutmelding werd opgevangen: ' . $e;
+}
+
+
+?>

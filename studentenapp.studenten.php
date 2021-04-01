0ca8750bc7cd3b998bda4f373c37b44bdb06f80a
scalda-nvinke <nick_vinke@student.scalda.nl>
2021-04-01 14:35:32 +0200

    add student crud table html

--- studentenapp.studenten.php
+++ studentenapp.studenten.php
@@ -13,11 +13,41 @@ include 'includes/header.php';
 <main class="container">
 
   <div class="starter-template text-center py-5 px-3">
-    <h1>Welkom op de Studenten Webapplicatie</h1>
-    <p class="lead">Op deze applicatie kun je studenten, docenten en vakkenpakketten bijhouden.</p>
+    <h1>De studentenlijst</h1>
   </div>
 
-</main><!-- /.container -->
+  <div class="row table-responsive">
+    <table class="table table-striped">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Naam</th>
+          <th scope="col">Email</th>
+          <th scope="col">Telefoonnummer</th>
+          <th scope="col">Vakkenpakket</th>
+          <th scope="col">Actie</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php //foreach ($students as $student):?>
+        <tr>
+          <td><?php ?></td>
+          <td><?php ?></td>
+          <td><?php ?></td>
+          <td><?php ?></td>
+          <td><?php ?></td>
+          <td class="actions">
+            <a href="includes/student" data-toggle="tooltip" title="Bekijken"><i class="fas fa-info-circle"></i></a>
+            <a href="" data-toggle="tooltip" title="Bewerken"><i class="far fa-edit"></i></a>
+            <a href="" data-toggle="tooltip" title="Verwijderen"><i class="far fa-trash-alt"></i></a>
+          </td>
+        </tr>
+        <?php //end foreach;?>
+      </tbody>
+    </table>
+  </div>
+
+</main>
 
 
 <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js"
